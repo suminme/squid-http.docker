bd3ab2d27d8f9ebd9bfcc7366670c6fb7653d6a9
SUMIN <suminme@users.noreply.github.com>
2016-04-05 21:45:41 +0800

    Update squid.sh

--- squid.sh
+++ squid.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+#conf
+cd /etc/squid3
+rm -rf squid.conf
+
+echo "http_port 3128
+http_access allow localhost
+http_access allow all
+"  >> /etc/squid3/squid.conf
+
+#restart
 cd /usr/sbin
 squid3 -k shutdown
 squid3 -N -d1
